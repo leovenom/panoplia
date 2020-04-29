a7bfa123e13c372c742844981ce9cede20cbab75
leovenom <leonardt@oputlook.com>
2020-04-29 17:23:21 +0100

    testando  tudo

--- app/models/shott.rb
+++ app/models/shott.rb
@@ -1,6 +1,5 @@
 class Shott < ApplicationRecord
   belongs_to :user
-  belongs_to :entity
   has_many :comments, dependent: :destroy
   
   mount_uploader :user_shott, UserShottUploader
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  
-  get 'profiles/show'
 resources :shotts do 
   	resources :comments 
   	member do 
