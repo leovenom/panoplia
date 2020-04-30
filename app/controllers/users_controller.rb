a4e1265766be9f4509b0fbe31730c918e023040a
leovenom <leonardt@oputlook.com>
2020-04-30 20:45:14 +0100

    show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :check_if_logged_in, only: [:show, :edit, :update, :delete]
-
   def show
     @user = User.find(params[:id])
     @user_shotts  = @user.shotts
