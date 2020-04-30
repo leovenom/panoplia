0cab7ac60547a9b5ed1b3746f1f042ff0ebc97f3
leovenom <leonardt@oputlook.com>
2020-04-30 21:25:04 +0100

    edit change avatar - ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,
     if: :devise_controller?
 
-  def check_if_logged_in
-    deny_access unless signed_in?
-  end
-      
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])  
   end
 end
