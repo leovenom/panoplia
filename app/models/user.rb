ebaee0e3d6f183c6268bfcd506d0ba68c5b169cf
leovenom <leonardt@oputlook.com>
2020-05-07 16:24:43 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,27 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   acts_as_voter   
   has_one_attached :avatar
-  after_commit :add_defalut_avatar, on: %i[create update]
+  # after_commit :add_defalut_avatar, on: %i[create update]
 
-  def avatar_thumbnail
-    if avatar.attached?
-      avatar.variant(resize: '150x150!').processed
-    else
-      '/default_profile.jpg'
-    end
-  end
+  # def avatar_thumbnail
+  #   if avatar.attached?
+  #     avatar.variant(resize: '150x150!').processed
+  #   else
+  #     '/default_profile.jpg'
+  #   end
+  # end
 
-  private
-  def add_defalut_avatar
-    unless avatar.attached?
-      avatar.attached(
-        io: File.open(
-          Rails.root.join(
-            'app', 'assets', 'images', 'default_profile.jpg'
-          )
-        ), filename: 'default_profile.jpg',
-        content_type: 'image/jpg'
-      )
-    end
-  end
+  # private
+  # def add_defalut_avatar
+  #   unless avatar.attached?
+  #     avatar.attached(
+  #       io: File.open(
+  #         Rails.root.join(
+  #           'app', 'assets', 'images', 'default_profile.jpg'
+  #         )
+  #       ), filename: 'default_profile.jpg',
+  #       content_type: 'image/jpg'
+  #     )
+  #   end
+  # end
 end
